7a101e9c7e49d1714842b9537303d302cf2698f4
Sergey Zemskov <Sergey Zemskov>
2016-11-12 01:15:43 +0300

    1

--- src/index.js
+++ src/index.js
@@ -14,6 +14,17 @@ app.get('/T2A', (req, res) => {
     res.send(summ.toString());
 });
 
+app.get('/T2B', (req, res) => {
+    var arr = req.query.fullname.split(' ');
+    var str = '';
+    for (var i = 0; i < arr.length; i++) {
+      if (i=0) str = str + arr[i]
+      else str = str + str.charAt(0) + '.';
+    }
+}   if (i>3) res.send('Invalid fullname');
+});
+
+
 app.listen(3000, () => {
   console.log('Your app listening on port 3000!');
 });
